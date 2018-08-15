20f7639e882290459b8b3f29f1bf9eba3af96399
Israel3D <israelmesquita07@gmail.com>
2018-08-15 02:14:24 -0300

    tableView Infinita

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -3,13 +3,6 @@ import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-an
 import { MoovieProvider } from "../../providers/moovie/moovie";
 import { FilmeDetalhesPage } from "../filme-detalhes/filme-detalhes";
 
-/**
- * Generated class for the FeedPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-feed',
@@ -30,6 +23,8 @@ export class FeedPage {
   };
 
   public lista_filmes = new Array<any>();
+  public page = 1;
+  public infiniteScroll;
   public loader;
   public refresher;
   public isRefreshing: boolean = false;
@@ -68,16 +63,27 @@ export class FeedPage {
     alert(num1+num2);
   }
 
+  public doInfinite(infiniteScroll) {
+    this.page++;
+    this.infiniteScroll = infiniteScroll;
+    this.carregarFilmes(true);
+  }
+
   ionViewDidEnter(){
     this.carregarFilmes();
   }
 
-  carregarFilmes() {
+  carregarFilmes(newpage: boolean = false) {
     this.abrirCarregando();
-    this.movieProvider.getLatestMovies().subscribe(data=>{
+    this.movieProvider.getLatestMovies(this.page).subscribe(data=>{
       const response = JSON.parse((data as any)._body);
-      this.lista_filmes = response.results;
-      console.log(response);
+
+      if (newpage) {
+        this.lista_filmes = this.lista_filmes.concat(response.results);
+        this.infiniteScroll.complete();
+      } else {
+        this.lista_filmes = response.results;
+      }
       this.fecharCarregando();
 
       if (this.isRefreshing) {
--- src/providers/moovie/moovie.ts
+++ src/providers/moovie/moovie.ts
@@ -2,27 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-/*
-  Generated class for the MoovieProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class MoovieProvider {
 
   private baseUrl = "https://api.themoviedb.org/3";
   private chaveAPI = "dfefa0dcb9ee40d95a920753d6e62a44";
   private languageUrl = "&language=pt-BR";
-  private urlAPI = this.baseUrl+"/movie/popular?api_key="+this.chaveAPI+this.languageUrl;
   
   constructor(public http: Http) {
     console.log('Hello MoovieProvider Provider');
   }
    
    //get method implementation
-  public getLatestMovies() {
-    return this.http.get(this.urlAPI);
+  public getLatestMovies(page = 1) {
+    return this.http.get(this.baseUrl+`/movie/popular?page=${page}&api_key=`+this.chaveAPI+this.languageUrl);
   }
 
   public getLatestMovieDetalhes(filmeID) {
